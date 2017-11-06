5d372254475502a40bd053f96c224f3d0937ef2b
jh2633 <joseph.jr@gmail.com>
2017-11-06 16:58:11 +0000

    first commit, project setup with lib + spc"

---
+++ lib/tube.rb
@@ -0,0 +1,9 @@
+require_relative 'stations'
+include StationNames
+
+class Tube
+
+    def list_of_stations
+    end
+
+end
---
+++ spec/tube_spec.rb
@@ -0,0 +1,12 @@
+require 'rspec'
+require_relative '../lib/tube'
+
+describe Tube do
+
+  describe 'tube class' do
+    it "returns a list of all stations in London" do
+
+    end
+  end
+
+end
