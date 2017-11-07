7eed28265904556c7c3dc24816ff7111df4c3914
jh2633 <joseph.jr@gmail.com>
2017-11-07 16:54:58 +0000

    add private methods for creating a list of alphabet characters, and splitting all strings in an array of strings

--- lib/tube.rb
+++ lib/tube.rb
@@ -3,7 +3,31 @@ include StationNames
 
 class Tube
 
-    def list_of_stations
+    def list_of_stations(station_names)
+      split_stations = split_strings(station_names)
+      alphabets = generate_alphabet_array
+      shortest_list = []
+
+      return shortest_list
     end
+
+    def compare_arrays
+      
+    end
+
+
+private
+
+  def generate_alphabet_array
+    ('a'..'z').to_a
+  end
+
+  def split_strings(array)
+    array_of_split_strings = []
+    array.each do |x|
+       array_of_split_strings << x.split(//)
+     end
+    return array_of_split_strings
+  end
 
 end
--- spec/tube_spec.rb
+++ spec/tube_spec.rb
@@ -3,10 +3,14 @@ require_relative '../lib/tube'
 
 describe Tube do
 
-  describe 'tube class' do
-    it "returns a list of all stations in London" do
-
+  describe 'list of statons' do
+    it "returns an array" do
+      stations = ["oxford circus", "euston"]
+      t = Tube.new
+      expect(t.list_of_stations(stations)).to be_an_instance_of(Array)
     end
+
+
   end
 
 end
