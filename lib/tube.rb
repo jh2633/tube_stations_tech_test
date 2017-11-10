f3c916aeb5abeaf6a14e5d36e5d9b8d81e75d027
jh2633 <joseph.jr@gmail.com>
2017-11-10 13:09:34 +0000

    add tests for iteration method, moving methods to private

--- lib/tube.rb
+++ lib/tube.rb
@@ -3,9 +3,9 @@ include StationNames
 
 class Tube
   attr_reader :stations_array
-    def initialize(hash = StationNames::ALL_STATIONS, alphabet = ('a'..'z').to_a)
+    def initialize(hash = StationNames::ALL_STATIONS, arr = [], alphabet = ('a'..'z').to_a)
       @stations_hash = hash
-      @stations_array = []
+      @stations_array = arr
       @alphabet_array = alphabet
     end
 
@@ -20,31 +20,29 @@ class Tube
       iterate_array(split_stations, @alphabet_array)
     end
 
-    #extracting the block from list of stations, this will compare two arrays,
-    #returning the shortest list of elements in the first array that matches the
-    #elements from the second array
-    def iterate_array(array_one, array_two)
-      shortest_list = []
-      array_one.each do |x|
-        if compare_arrays(x, array_two)
-          next
-        else
-          array_two -= x
-          shortest_list << x.join("")
-        end
-      end
-      return shortest_list
-
-    end
-
-    #checks for similiarity between two stations
-    def compare_arrays(one, two)
-      (one&two).empty?
-    end
-
 
 private
+  #checks for similiarity between two stations
+  def compare_arrays(one, two)
+    (one&two).empty?
+  end
 
+  #extracting the block from list of stations, this will compare two arrays,
+  #returning the shortest list of elements in the first array that matches the
+  #elements from the second array
+  def iterate_array(array_one, array_two)
+    shortest_list = []
+    array_one.each do |x|
+      if compare_arrays(x, array_two)
+        next
+      else
+        array_two -= x
+        shortest_list << x.join("")
+      end
+    end
+    return shortest_list
+
+  end
 
   #split an array of strings into sub-arrays of individual characters
   def split_strings(array)
--- spec/tube_spec.rb
+++ spec/tube_spec.rb
@@ -2,29 +2,48 @@ require 'rspec'
 require_relative '../lib/tube'
 
 describe Tube do
-
+  stations = ["oxford circus", "euston", "brixton", "hammersmith"]
+  before(:each) do
+     @tube = Tube.new
+   end
   describe 'list of statons' do
     it "returns an array" do
-      stations = ["oxford circus", "euston"]
-      t = Tube.new
-      expect(t.list_of_stations(stations)).to be_an_instance_of(Array)
+      expect(@tube.list_of_stations(stations)).to be_an_instance_of(Array)
     end
 
   end
 
   describe 'compare arrays' do
+    array_1 = [1,2,3]
+    array_2 = [1,2]
+    array_3 = [4]
+
     it "returns false if arrays contain same elements" do
-      t = Tube.new
-      array_1 = [1,2,3]
-      array_2 = [1,2]
-      expect(t.compare_arrays(array_1, array_2)).to be(false)
+      expect(@tube.send(:compare_arrays, array_1, array_2)).to be(false)
     end
     it "returns true if arrays don't have overlapping elements" do
-      t = Tube.new
-      array_1 = [1,2,3]
-      array_2 = [4]
-      expect(t.compare_arrays(array_1, array_2)).to be(true)
+      expect(@tube.send(:compare_arrays, array_1, array_3)).to be(true)
     end
   end
+
+  describe 'split strings' do
+    arr = ["hello", "hi"]
+    arr_two = [["h","e","l","l","o"],["h","i"]]
+
+      it "takes an array of strings and split them into individual characters,
+      in separate arrays" do
+        expect(@tube.send(:split_strings, arr)).to eq(arr_two)
+      end
+    end
+
+    describe 'iterate array method' do
+      arr_two = [["h","e","l","l","o"],["h","i"],["j","e","a","n","s"]]
+      i = ["i","j"]
+      it "returns an array of string(s) that constitutes the second array in the argument" do
+        expect(@tube.send(:iterate_array, arr_two, i)).to eq(["hi", "jeans"])
+      end
+    end
+
+
 
 end
