1503bc36d32fe57cdc369ad7da7b6873f52f337c
jh2633 <joseph.jr@gmail.com>
2017-11-10 13:33:35 +0000

    update instructions in README

--- lib/tube.rb
+++ lib/tube.rb
@@ -12,10 +12,10 @@ class Tube
     #create an array of station name from the hash"
     def create_station_array
       @stations_hash.each {|k,v| @stations_array << v}
-      @stations_array.sort! {|x, y| y.length <=> x.length}
     end
 
     def list_of_stations(station_names = @stations_array)
+      station_names.sort! {|x, y| y.length <=> x.length}
       split_stations = split_strings(station_names)
       iterate_array(split_stations, @alphabet_array)
     end
